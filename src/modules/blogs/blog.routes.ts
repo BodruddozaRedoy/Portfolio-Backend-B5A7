633d30982b2c6dc846e8fd41134adee826437bec
bodruddozaredoy <bodruddozaredoy@gmail.com>
2025-10-01 01:12:03 +0600

    feat(blogs): change update route from PUT to PATCH

--- src/modules/blogs/blog.routes.ts
+++ src/modules/blogs/blog.routes.ts
@@ -6,7 +6,7 @@ const router = Router();
 router.get("/", BlogController.getBlogs);
 router.get("/:id", BlogController.getBlogById);
 router.post("/", BlogController.createBlog);
-router.put("/:id", BlogController.updateBlog);
+router.patch("/:id", BlogController.updateBlog);
 router.delete("/:id", BlogController.deleteBlog);
 
 export const blogRoutes = router;
