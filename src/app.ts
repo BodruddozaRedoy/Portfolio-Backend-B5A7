21bb1c57f9956b86703be54a2c7a8de04df3d96e
bodruddozaredoy <bodruddozaredoy@gmail.com>
2025-10-07 05:00:59 +0600

    update app.ts

--- src/app.ts
+++ src/app.ts
@@ -11,7 +11,7 @@ dotenv.config()
 
 app.use(express.json());
 app.use(cors({
-  origin: ["http://localhost:3000"]
+  origin: ["http://localhost:3000", "https://portfolio-frontend-b5-a7.vercel.app"]
 }))
 
 // Register routes
